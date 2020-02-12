a2bc262488675c8b4e6ed32a47087b6a3dd38443
Clayton Dean <clay_holman8@yahoo.com>
2020-02-12 09:12:05 -0700

    final fixes, all things working

--- js.js
+++ js.js
@@ -1,28 +1,47 @@
-var seconds = 100;
-var count = document.getElementById("counter");
 
+
+var seconds = 100;
+var score = 0;
+var count = document.getElementById("counter");
+var submit_btn = document.getElementById("submit");
 
 function decreaseSeconds() {
-    seconds -= 1;
-    count.innerText = "Score " + seconds;
+		if (seconds > 0){
+    	seconds -= 1;
+    	count.innerText = "Time left: " + seconds + " secs";
+    }else{
+    	//End Quiz
+    }
+}
 
+function getQValue(form, name) {
+    var val;
+    var radios = form.elements[name];
+    
+    for (var i=0, len=radios.length; i<len; i++) {
+        if ( radios[i].checked ) { 
+            val = radios[i].value; 
+            break; 
+        }
+    }
+    return val;
 }
 
 var Score = setInterval(decreaseSeconds, 1000);
 
 
-
-
-function myFunction() {
-   count = count + answer;
-  } 
-  
-
-
 submit_btn.onclick = function() {
-	var ch = document.getElementById('q1').checked;
-    count = count + parseInt(ch.value);
-    ch.
-   alert("Here is your score: " + count);
-   count = 0; // so we can start quiz over
+	
+	//alert("Here is your score: " + score);
+    var q1 = getQValue(document.getElementById('q1'),'q1');
+    var q2 = getQValue(document.getElementById('q2'),'q2');
+    var q3= getQValue(document.getElementById('q3'),'q3');
+    q1 = parseInt(q1);
+    q2 = parseInt(q2);
+    q3 = parseInt(q3);
+    
+    total = q1 + q2 + q3;
+    alert("Score is: " + total + " out of 3");
+	score = 0; // so we can start quiz over
 };
+
